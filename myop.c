77d51f78af22080c8762ef2c0a364fac215e7747
Ardat-Giggsy <liurui@cs.unc.edu>
2014-04-27 23:55:42 -0400

    A Working Scheduler

--- myop.c
+++ myop.c
@@ -7,26 +7,61 @@
 #include <linux/module.h>
 #include <linux/init.h>
 
-struct myop_data {
-	struct list_head queue;
-};
+static const int read_weight = 1;
+static const int write_weight = 1;
 
-static void myop_merged_requests(struct request_queue *q, struct request *rq,
-				 struct request *next)
-{
-	list_del_init(&next->queuelist);
-}
+struct myop_data {
+	struct list_head read_queue;
+	struct list_head write_queue;
+	int dir;
+	int weight;
+};
 
 static int myop_dispatch(struct request_queue *q, int force)
 {
 	struct myop_data *nd = q->elevator->elevator_data;
+	const int data_dir = nd -> dir;
 
-	if (!list_empty(&nd->queue)) {
-		struct request *rq;
-		rq = list_entry(nd->queue.next, struct request, queuelist);
-		list_del_init(&rq->queuelist);
-		elv_dispatch_sort(q, rq);
-		return 1;
+	if(data_dir==READ){
+		if(!list_empty(&nd->read_queue)){
+			struct request *rq;
+			rq = list_entry(nd->read_queue.next, struct request, queuelist);
+			list_del_init(&rq->queuelist);
+			elv_dispatch_sort(q, rq);
+			nd -> weight = nd -> weight - 1;
+			if(nd -> weight == 0)
+			{
+				nd->dir=WRITE;
+				nd->weight = write_weight;
+			}
+			return 1;
+		}else if(!list_empty(&nd->write_queue)){
+			struct request *rq;
+			rq = list_entry(nd->write_queue.next, struct request, queuelist);
+			list_del_init(&rq->queuelist);
+			elv_dispatch_sort(q, rq);
+			return 1;
+		}
+	}else{
+		if(!list_empty(&nd->write_queue)){
+			struct request *rq;
+			rq = list_entry(nd->write_queue.next, struct request, queuelist);
+			list_del_init(&rq->queuelist);
+			elv_dispatch_sort(q, rq);
+			nd -> weight = nd -> weight - 1;
+			if(nd -> weight == 0)
+			{
+				nd->dir = READ;
+				nd->weight = read_weight;
+			}
+			return 1;
+		}else if(!list_empty(&nd->read_queue)){
+			struct request *rq;
+			rq = list_entry(nd->read_queue.next, struct request, queuelist);
+			list_del_init(&rq->queuelist);
+			elv_dispatch_sort(q, rq);
+			return 1;
+		}
 	}
 	return 0;
 }
@@ -34,35 +69,20 @@ static int myop_dispatch(struct request_queue *q, int force)
 static void myop_add_request(struct request_queue *q, struct request *rq)
 {
 	struct myop_data *nd = q->elevator->elevator_data;
-
-	list_add_tail(&rq->queuelist, &nd->queue);
+	const int data_dir = rq_data_dir(rq);
+	
+	if(data_dir==READ){
+		list_add_tail(&rq->queuelist, &nd->read_queue);
+	}else{
+		list_add_tail(&rq->queuelist, &nd->write_queue);
+	}
 }
 
 static int myop_queue_empty(struct request_queue *q)
 {
 	struct myop_data *nd = q->elevator->elevator_data;
 
-	return list_empty(&nd->queue);
-}
-
-static struct request *
-myop_former_request(struct request_queue *q, struct request *rq)
-{
-	struct myop_data *nd = q->elevator->elevator_data;
-
-	if (rq->queuelist.prev == &nd->queue)
-		return NULL;
-	return list_entry(rq->queuelist.prev, struct request, queuelist);
-}
-
-static struct request *
-myop_latter_request(struct request_queue *q, struct request *rq)
-{
-	struct myop_data *nd = q->elevator->elevator_data;
-
-	if (rq->queuelist.next == &nd->queue)
-		return NULL;
-	return list_entry(rq->queuelist.next, struct request, queuelist);
+	return (list_empty(&nd->read_queue) || list_empty(&nd->write_queue));
 }
 
 static void *myop_init_queue(struct request_queue *q)
@@ -72,7 +92,10 @@ static void *myop_init_queue(struct request_queue *q)
 	nd = kmalloc_node(sizeof(*nd), GFP_KERNEL, q->node);
 	if (!nd)
 		return NULL;
-	INIT_LIST_HEAD(&nd->queue);
+	INIT_LIST_HEAD(&nd->read_queue);
+	INIT_LIST_HEAD(&nd->write_queue);
+	nd->dir = WRITE;
+	nd->weight = write_weight; 
 	return nd;
 }
 
@@ -80,18 +103,16 @@ static void myop_exit_queue(struct elevator_queue *e)
 {
 	struct myop_data *nd = e->elevator_data;
 
-	BUG_ON(!list_empty(&nd->queue));
+	BUG_ON(!list_empty(&nd->read_queue));
+	BUG_ON(!list_empty(&nd->write_queue));
 	kfree(nd);
 }
 
 static struct elevator_type elevator_myop = {
 	.ops = {
-		.elevator_merge_req_fn		= myop_merged_requests,
 		.elevator_dispatch_fn		= myop_dispatch,
 		.elevator_add_req_fn		= myop_add_request,
 		.elevator_queue_empty_fn	= myop_queue_empty,
-		.elevator_former_req_fn		= myop_former_request,
-		.elevator_latter_req_fn		= myop_latter_request,
 		.elevator_init_fn		= myop_init_queue,
 		.elevator_exit_fn		= myop_exit_queue,
 	},
@@ -117,4 +138,4 @@ module_exit(myop_exit);
 
 MODULE_AUTHOR("Rui Liu");
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("my I/O scheduler to do something different from what's already in Linux");
+MODULE_DESCRIPTION("A two-queue I/O Scheduler");
